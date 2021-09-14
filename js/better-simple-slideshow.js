e8a314547c7b07922e18e7ce4ca6ae035f6c480d
Maddi <57687290+mxddi@users.noreply.github.com>
2021-09-14 16:03:42 -0700

    Resolved hammer.min.js "file not found" error

--- js/better-simple-slideshow.js
+++ js/better-simple-slideshow.js
@@ -1,17 +1,3 @@
-/* var makeBSS=function(a,b){var c=document.querySelectorAll(a),d={},e={init:function(a,b){this.counter=0,this.el=a,this.$items=a.querySelectorAll("figure"),this.numItems=this.$items.length,b=b||{},b.auto=b.auto||!1,this.opts={auto:"undefined"==typeof b.auto?!1:b.auto,speed:"undefined"==typeof b.auto.speed?1500:b.auto.speed,pauseOnHover:"undefined"==typeof b.auto.pauseOnHover?!1:b.auto.pauseOnHover,fullScreen:"undefined"==typeof b.fullScreen?!1:b.fullScreen,swipe:"undefined"==typeof b.swipe?!1:b.swipe},this.$items[0].classList.add("bss-show"),this.injectControls(a),this.addEventListeners(a),this.opts.auto&&this.autoCycle(this.el,this.opts.speed,this.opts.pauseOnHover),this.opts.fullScreen&&this.addFullScreen(this.el),this.opts.swipe&&this.addSwipe(this.el)},showCurrent:function(a){this.counter=a>0?this.counter+1===this.numItems?0:this.counter+1:this.counter-1<0?this.numItems-1:this.counter-1,[].forEach.call(this.$items,function(a){a.classList.remove("bss-show")}),this.$items[this.counter].classList.add("bss-show")},injectControls:function(a){var b=document.createElement("span"),c=document.createElement("span"),d=document.createDocumentFragment();b.classList.add("bss-prev"),c.classList.add("bss-next"),b.innerHTML="&#10094",c.innerHTML="&#10095",d.appendChild(b),d.appendChild(c),a.appendChild(d)},addEventListeners:function(a){var b=this;a.querySelector(".bss-next").addEventListener("click",function(){b.showCurrent(1)},!1),a.querySelector(".bss-prev").addEventListener("click",function(){b.showCurrent(-1)},!1),a.onkeydown=function(a){a=a||window.event,37===a.keyCode?b.showCurrent(-1):39===a.keyCode&&b.showCurrent(1)}},autoCycle:function(a,b,c){var d=this,e=window.setInterval(function(){d.showCurrent(1)},b);c&&(a.addEventListener("mouseover",function(){e=clearInterval(e)},!1),a.addEventListener("mouseout",function(){e=window.setInterval(function(){d.showCurrent(1)},b)},!1))},addFullScreen:function(a){var b=this,c=document.createElement("span");c.classList.add("bss-fullscreen"),a.appendChild(c),a.querySelector(".bss-fullscreen").addEventListener("click",function(){b.toggleFullScreen(a)},!1)},
-
- addSwipe:function(a){
-    var b=this,
-    c=new Hammer(a);
-    c.on("swiperight",function(){
-        b.showCurrent(-1)}),
-        
-    c.on("swipeleft",function(){
-        b.showCurrent(1)})
-},
-
-toggleFullScreen:function(a){document.fullscreenElement||document.mozFullScreenElement||document.webkitFullscreenElement||document.msFullscreenElement?document.exitFullscreen?document.exitFullscreen():document.msExitFullscreen?document.msExitFullscreen():document.mozCancelFullScreen?document.mozCancelFullScreen():document.webkitExitFullscreen&&document.webkitExitFullscreen():document.documentElement.requestFullscreen?a.requestFullscreen():document.documentElement.msRequestFullscreen?a.msRequestFullscreen():document.documentElement.mozRequestFullScreen?a.mozRequestFullScreen():document.documentElement.webkitRequestFullscreen&&a.webkitRequestFullscreen(a.ALLOW_KEYBOARD_INPUT)}};[].forEach.call(c,function(a){d=Object.create(e),d.init(a,b)})}; */
-
 var makeBSS = function (el, options) {
     var $slideshows = document.querySelectorAll(el), // a collection of all of the slideshow
         $slideshow = {},
@@ -22,9 +8,9 @@ var makeBSS = function (el, options) {
                 options.auto = options.auto || false; // if options.auto object not passed in, then set to false
                 this.opts = {
                     selector: (typeof options.selector === "undefined") ? "figure" : options.selector,
-                    auto: (typeof options.auto === "undefined") ? false : options.auto,
-                    speed: (typeof options.auto.speed === "undefined") ? 1500 : options.auto.speed,
-                    pauseOnHover: (typeof options.auto.pauseOnHover === "undefined") ? false : options.auto.pauseOnHover,
+                    auto: true,
+                    speed: 4000,
+                    pauseOnHover: true,
                     fullScreen: /* (typeof options.fullScreen === "undefined") ? false : options.fullScreen, */ false,
                     swipe: /* (typeof options.swipe === "undefined") ? false : options.swipe */ true
                 };
@@ -75,8 +61,8 @@ var makeBSS = function (el, options) {
                 spanNext.classList.add('bss-next');
         
                 // add contents
-                spanPrev.innerHTML = '&laquo;';
-                spanNext.innerHTML = '&raquo;';
+                spanPrev.innerHTML = '&#10094;';
+                spanNext.innerHTML = '&#10095;';
                 
                 // append elements to fragment, then append fragment to DOM
                 docFrag.appendChild(spanPrev);
@@ -135,7 +121,7 @@ var makeBSS = function (el, options) {
             },
             addSwipe: function(el){
                 var that = this,
-                    ht = new Hammer(el);
+                ht = new Hammer(el);
                 ht.on('swiperight', function(e) {
                     that.showCurrent(-1); // decrement & show
                 });
